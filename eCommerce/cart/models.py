63c297dfd6b337ee571595ad43860d03ade3fe56
mohsinmdl <mohsinmahmoodmdl@gmail.com>
2018-09-17 00:38:02 +0500

    067 m2m changes signal to calculate cart total

--- eCommerce/cart/models.py
+++ eCommerce/cart/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.conf import settings
+from django.db.models.signals import pre_save, post_save, m2m_changed
+
 from products.models import Product
+
 # Create your models here.
 
 User = settings.AUTH_USER_MODEL
+
 
 class CartManager(models.Manager):
     def new_or_get(self, request):
@@ -28,10 +32,12 @@ class CartManager(models.Manager):
                 user_obj = user
         return self.model.objects.create(user=user_obj)
 
+
 class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True , blank=True)
-    products = models.ManyToManyField(Product,blank=True)
-    total = models.DecimalField(max_digits=100 ,decimal_places=2 ,default=0.00 )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    products = models.ManyToManyField(Product, blank=True)
+    subtotal = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+    total = models.DecimalField(max_digits=100, decimal_places=2, default=0.00)
     updated = models.DateTimeField(auto_now=True)
     timestamps = models.DateTimeField(auto_now_add=True)
 
@@ -40,3 +46,23 @@ class Cart(models.Model):
     def __str__(self):
         return str(self.id)
 
+
+def m2m_changed_cart_receiver(sender, instance, action, *args, **kwargs):
+    if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+        products = instance.products.all()
+        total = 0
+        for x in products:
+            total += x.price
+        if instance.subtotal != total:
+            instance.subtotal = total
+            instance.save()
+
+
+m2m_changed.connect(m2m_changed_cart_receiver, sender=Cart.products.through)
+
+
+def pre_save_cart_receiver(sender, instance, *args, **kwargs):
+    instance.total = instance.subtotal + 10  # * 1.08
+
+
+pre_save.connect(pre_save_cart_receiver, sender=Cart)
